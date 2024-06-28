f5a3b137787d5b5da0c8b5ba073d798f60dbce98
misha <misha@FX503VD-eb7009bd.localdomain>
2024-06-28 14:49:20 +0000

    Home’ tab

--- src/components/body/colaB/ColaBody.tsx
+++ src/components/body/colaB/ColaBody.tsx
@@ -7,13 +7,19 @@ const ColaBody: React.FC = () => {
         <div>
             <div className="cola3d">
                 <div className="PColaText">
-                    {ColaText.map((text: { text: string }, index) => index === 2 ? (
-                        <div key={index} className="textWithImage">
-                            <img className='ColaImage' src={ColaImage[1].img} alt="Cola" />
+                    {ColaText.map((text: { text: string }, index) => index === 0 ? (
+                        <div key={index} className="textWithImage" style={{ display: 'flex' }}>
+                            <img className='ColaImage121' src={ColaImage[1].img} alt="Cola" />
                             <p className='cola'>{text.text}</p>
                         </div>
                     ) : (
+                        <div className="shopm">
                         <p key={index} className={index === 1 ? 'colaH1' : 'cola'}>{text.text}</p>
+                        <div className="sop">
+                            <button className='Shop'>Shop Now</button>
+                            <button className='Learm'>Learm More</button>
+                        </div>
+                        </div>
                     ))}
                 </div>
                 <img className='ColaImage' src={ColaImage[0].img} />
